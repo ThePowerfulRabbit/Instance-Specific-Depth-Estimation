19abea640dd926374748a302fb2d04ec6f2e1464
Pukar Shiwakoti <shiwakotipukar2061@gmail.com>
2026-09-05 19:05:06 +0545

    we get class id and class name for each loop now

--- masklabel.py
+++ masklabel.py
@@ -20,68 +20,69 @@ result = results[0] # result is a list with elements which can be accessed by in
 # print(type(result.masks.data))
 # print(result.masks.data.shape)
 
-# # This will print the mask with 1s and 0s and some floating point values in between 1 means the part of the image which has the particular object
+# # This will print the mask with values between 1s and 0s 
 # print(result.masks.data[0])
 # print(result.masks.data[0].shape)
 
-#lets get the object names from the model is a list
+original_image = result.orig_img #original image data
+image_height, image_width, channels = image.shape #original image dimensions
 
-print(result.boxes.cls[0].item())
+#getting the total number of masks/objects the model detected
+no_objects = result.masks.data.shape[0]
 
-print(result.names[0])
-# original_image = result.orig_img #original image data
-# image_height, image_width, channels = image.shape #original image dimensions
+#creating empty image with same dimension as original image which will b e the canvas for masks
+colored_mask = np.zeros_like(image)
 
-# #getting the total number of masks/objects the model detected
-# no_objects = result.masks.data.shape[0]
+def random_color(): #color generator for different masks
+    B = random.randint(0,255)
+    G = random.randint(0,255)
+    R = random.randint(0,255)
+    color =[B,G,R]
+    return color
 
-# #creating empty image with same dimension as original image which will b e the canvas for masks
-# colored_mask = np.zeros_like(image)
+for i in range (0, no_objects):
+    
+    #lets get the object names from the model is a list
+    class_id = int( result.boxes.cls[i].item() )
+    print("Class ID: ",class_id)
+    print("Class name: ",result.names[class_id])
+    
+    # Trying to convert the first mask into a boolean mask
+    num_mask = result.masks.data[i] #we take one 2d array representing one mask
+    BW_mask = (num_mask > 0.5) * 255 #any number greater than 0.5 = true * 255 = 255, less than 0.5 = false = 0 *255 = 0 so it created a new mask
+    # print(BW_mask)
 
-# def random_color(): #color generator for different masks
-#     B = random.randint(0,255)
-#     G = random.randint(0,255)
-#     R = random.randint(0,255)
-#     color =[B,G,R]
-#     return color
+    # There is a problem now the shape of the arrays of the mask and original images are not the same:
+    print("Original image shape:", original_image.shape)
+    print("Mask shape:", num_mask.shape)
+    # Original image shape: (836, 1254, 3)
+    # Mask shape: torch.Size([448, 640])
+    # also original shape is a numpy array and mask is a pytorch tensor
 
-# for i in range (0, no_objects):
-#     # Trying to convert the first mask into a boolean mask
-#     num_mask = result.masks.data[i] #we take one 2d array representing one mask
-#     BW_mask = (num_mask > 0.5) * 255 #any number greater than 0.5 = true * 255 = 255, less than 0.5 = false = 0 *255 = 0 so it created a new mask
-#     # print(BW_mask)
+    # So we need to resize the mask for which we will use a function of open CV: cv2.resize(source, (width, height))
+    # NOTE!!!! you need to convert the pytorch tensor into a numpy array before passing into the resize function
+    # image.shape → height, width
+    # cv2.resize  → width, height
+    print(type(BW_mask))
+    BW_mask = BW_mask.cpu().numpy().astype('uint8'); #pytorch to numpy , also typecasting int16 to uint8 because the resize function does not accept int16
+    print(type(BW_mask))
 
-#     # There is a problem now the shape of the arrays of the mask and original images are not the same:
-#     print("Original image shape:", original_image.shape)
-#     print("Mask shape:", num_mask.shape)
-#     # Original image shape: (836, 1254, 3)
-#     # Mask shape: torch.Size([448, 640])
-#     # also original shape is a numpy array and mask is a pytorch tensor
+    # Resized_mask = cv2.resize(BW_mask, (1254,836)) : not recommended to hardcode the width and height
+    Resized_mask = cv2.resize(BW_mask, (image_width, image_height))
+    print(Resized_mask.shape)
 
-#     # So we need to resize the mask for which we will use a function of open CV: cv2.resize(source, (width, height))
-#     # NOTE!!!! you need to convert the pytorch tensor into a numpy array before passing into the resize function
-#     # image.shape → height, width
-#     # cv2.resize  → width, height
-#     print(type(BW_mask))
-#     BW_mask = BW_mask.cpu().numpy().astype('uint8'); #pytorch to numpy , also typecasting int16 to uint8 because the resize function does not accept int16
-#     print(type(BW_mask))
+    # image[mask_condition] = value
+    colored_mask[Resized_mask > 0] = random_color() #open cv uses [B,G,R]
 
-#     # Resized_mask = cv2.resize(BW_mask, (1254,836)) : not recommended to hardcode the width and height
-#     Resized_mask = cv2.resize(BW_mask, (image_width, image_height))
-#     print(Resized_mask.shape)
+# blend the mask with original image 
+# cv2.addWeighted(src1, alpha, src2, beta, gamma)
+# src1  : image
+# alpha : 0.7
+# src2  : colored_mask
+# beta  : 0.3
+# gamma : 0
+output = cv2.addWeighted(image, 0.7, colored_mask, 0.3, 0)
 
-#     # image[mask_condition] = value
-#     colored_mask[Resized_mask > 0] = random_color() #Wherever Resized_mask > 0 is true, set the corresponding pixel in colored_mask to green.open cv uses [B,G,R]
-
-# # blend the mask with original image 
-# # cv2.addWeighted(src1, alpha, src2, beta, gamma)
-# # src1  : image
-# # alpha : 0.7
-# # src2  : colored_mask
-# # beta  : 0.3
-# # gamma : 0
-# output = cv2.addWeighted(image, 0.7, colored_mask, 0.3, 0)
-
-# cv2.imshow("Segmentation Output", output)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.imshow("Segmentation Output", output)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
